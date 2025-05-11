52186fa84fd306238f2f4673ed2938684a5b0a1d
Luiz Henrique <luizhlc02@gmail.com>
2025-05-11 03:08:53 -0300

    feat: criando projeto e adicionando home e nav bar

---
+++ src/app/home/nav-bar/nav-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-nav-bar',
+  imports: [ButtonModule],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.scss'
+})
+export class NavBarComponent {
+
+}
