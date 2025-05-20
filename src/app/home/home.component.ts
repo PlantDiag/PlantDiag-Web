997e295d9dbdee2513d2e16d2542f2bda376be8e
Luiz Henrique <luizhlc02@gmail.com>
2025-05-19 22:30:44 -0300

    feat: atualizando o layout inteiro da home e nav bar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { PanelModule } from 'primeng/panel';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ChartModule } from 'primeng/chart';
+import {WebcamModule} from 'ngx-webcam';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, PanelModule, CommonModule, FormsModule, ChartModule, WebcamModule, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+culturas = [
+  { label: 'Milho', value: 'milho' },
+  { label: 'Soja', value: 'soja' },
+  { label: 'Arroz', value: 'arroz' },
+];
 
+visible = false
+
+culturaSelecionada?: string;
+dataInicio?: Date;
+dataFim?: Date;
+
+dadosGrafico = {
+  labels: ['Saudável', 'Larva da Soja', 'Vaquinha da Soja'],
+  datasets: [ 
+    {
+      label: 'Status das Plantas',
+      backgroundColor: ['#4caf50', '#ff9800', '#f44336'],
+      data: [50, 20, 30]
+    }
+  ]
+};
 }
--- src/app/home/nav-bar/nav-bar.component.ts
+++ src/app/home/nav-bar/nav-bar.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { DrawerModule } from 'primeng/drawer';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [ButtonModule],
+  imports: [ButtonModule, DrawerModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+  nomeUsuario = 'Adm';
 
 }
